dbf1b375374625518ab5f631c03b87cd88a2e2b8
julian <illegal_burrito@live.com>
2022-04-06 10:40:22 -0700

    update styling

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -7,24 +7,22 @@ import '../css/main.css'
 export default class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className='main-container'>
 
-        <div className="jumbotron jumbotron-fluid">
-          <div className="container">
-            <h1 className="display-4">Weather Dood</h1>
-            <p className="lead">Check the weather around the world dood</p>
+        <div className="banner">
+          <div className="">
+            <h1 className="title">Weather Dood</h1>
+            <p className="subtitle">Check the weather around the world dood</p>
           </div>
         </div>
 
-        <div className="row">
-          <div className='c-select'>
-            <Cities store={ rootStore }/>
-          </div>
-          <div>
-            <Weather store={rootStore}/>
-          </div>
+        <div id='city-selector'>
+          <Cities store={rootStore} />
         </div>
 
+        <div className='weather-container'>
+          <Weather store={rootStore} />
+        </div>
       </div>
     );
   }
--- src/js/components/weather/weather.jsx
+++ src/js/components/weather/weather.jsx
@@ -8,16 +8,22 @@ export default class Weather extends React.Component {
 
   render(){
     return (
-      <div className="weather-container">
-          <div id="city-banner">
+      <div className="card weather-card">
+
+        <div className="card-header">
+            Today's Forcast
+        </div>
+
+        <div className="card-body body-flex">
+            <div id="city-banner">
               <div>
                   <img src="city.png" alt="city"
                   id="city-img"/>
               </div>
               <span>{this.props.city.name}</span>
-          </div>
-
-          <div className="temp-info">
+            </div>
+        
+            <div className="temp-info">
               <ul className="list-group list-group-flush">
                   <li className="list-group-item">
                       Temperature: {this.props.city.main.temp}°F
@@ -46,7 +52,7 @@ export default class Weather extends React.Component {
                   className="figure-img img-figure rounded"/>
               </div>
           </div>
-
+        </div>
       </div>
   );
   }
